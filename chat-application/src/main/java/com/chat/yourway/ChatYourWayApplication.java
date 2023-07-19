ac038d301601bb24d16f2fc0a5e55fdd22769920
Dmytro Trotsenko <trohae7@gmail.com>
2023-07-19 17:41:31 +0300

    Initial commit

---
+++ chat-application/src/main/java/com/chat/yourway/ChatYourWayApplication.java
@@ -0,0 +1,13 @@
+package com.chat.yourway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatYourWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatYourWayApplication.class, args);
+	}
+
+}
---
+++ chat-application/src/test/java/com/chat/yourway/ChatYourWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.yourway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatYourWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
