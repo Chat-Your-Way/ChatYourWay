14b8b2076fd4f3618a7737c7280dc15e34ca857d
Dmytro Trotsenko <trohae7@gmail.com>
2023-07-28 00:44:50 +0300

    Added config userDetailsService, authenticationProvider, authenticationManager and passwordEncoder

---
+++ src/main/java/com/chat/yourway/security/ApplicationConfig.java
@@ -0,0 +1,52 @@
+package com.chat.yourway.security;
+
+import com.chat.yourway.repository.ContactRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * {@link ApplicationConfig}
+ *
+ * @author Dmytro Trotsenko on 7/26/23
+ */
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final ContactRepository contactRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> contactRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Contact not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
