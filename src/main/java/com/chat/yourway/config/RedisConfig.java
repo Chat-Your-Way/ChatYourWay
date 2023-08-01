a8092fac1f2b9ab00f67773525c355fed76a639c
Dmytro Trotsenko <trohae7@gmail.com>
2023-08-02 00:42:00 +0300

    Created Redis configuration

---
+++ src/main/java/com/chat/yourway/config/RedisConfig.java
@@ -0,0 +1,47 @@
+package com.chat.yourway.config;
+
+import com.chat.yourway.model.Token;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * {@link RedisConfig}
+ *
+ * @author Dmytro Trotsenko on 8/1/23
+ */
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        var redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisPassword));
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Token> redisTemplate() {
+        RedisTemplate<String, Token> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+}
