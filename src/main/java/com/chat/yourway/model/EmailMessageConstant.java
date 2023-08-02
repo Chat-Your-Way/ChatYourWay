41ade562bc4bd6ae0c6bf409cd9f3b42c0969c42
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-08-02 15:06:48 +0300

    feat: prepare data for sending email

--- src/main/java/com/chat/yourway/model/EmailMessageConstant.java
+++ src/main/java/com/chat/yourway/model/EmailMessageConstant.java
@@ -5,4 +5,5 @@ public class EmailMessageConstant {
     public static final String VERIFY_ACCOUNT_TEXT = "Dear, %s! Thank you for creating an account in out service.\n" +
             "Please click the link down bellow for verifying account.\n" +
             "Link: %s";
+    public static final String TOKEN_PARAMETER = "?token=";
 }
