4a96380ae69fbd6f3346be3f2258c39d44197eca
Dmytro Trotsenko <trohae7@gmail.com>
2023-08-04 23:57:30 +0300

    Removed loginPage for testing in swagger

--- src/main/java/com/chat/yourway/config/SecurityFilterConfig.java
+++ src/main/java/com/chat/yourway/config/SecurityFilterConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -56,9 +57,7 @@ public class SecurityFilterConfig {
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .formLogin(form -> form
-                        .loginPage("/auth/login")
-                        .permitAll()
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
                 )
                 .logout(logout -> logout
                         .logoutUrl("/auth/logout")
