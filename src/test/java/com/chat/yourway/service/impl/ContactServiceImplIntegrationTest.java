f33391f6711107a7c2e5dbdb8391682da53d819f
dmytro <dimastel7@gmail.com>
2023-08-07 22:16:10 +0300

    fix: create unit+integration tests for method changing password, set up h2 database for test profile

---
+++ src/test/java/com/chat/yourway/service/impl/ContactServiceImplIntegrationTest.java
@@ -0,0 +1,67 @@
+package com.chat.yourway.service.impl;
+
+import com.chat.yourway.dto.request.ChangePasswordDto;
+import com.chat.yourway.exception.NoEqualsPasswordException;
+import com.chat.yourway.model.Contact;
+import com.chat.yourway.repository.ContactRepository;
+import com.chat.yourway.service.ContactService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ContactServiceImplIntegrationTest {
+    private Contact contact;
+    @Autowired
+    private ContactService contactService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @BeforeEach
+    public void setUp() {
+        contact = Contact.builder()
+                .username("username12353")
+                .email("user@gmail.com")
+                .password(passwordEncoder.encode("oldPassword"))
+                .isActive(true)
+                .isPrivate(true)
+                .build();
+    }
+
+    @Test
+    public void testChangePassword_Success() {
+        ChangePasswordDto request = new ChangePasswordDto("oldPassword", "newPassword");
+
+        contactRepository.save(contact);
+        contactService.changePassword(request, contact);
+
+        Contact updatedContact = contactRepository.findByUsername(contact.getUsername())
+                .orElseGet(() -> contact);
+
+        assertTrue(passwordEncoder.matches("newPassword", updatedContact.getPassword()));
+    }
+
+    @Test
+    public void testChangePassword_InvalidOldPassword() {
+        ChangePasswordDto request = new ChangePasswordDto("errorPassword", "newPassword");
+
+        contactRepository.save(contact);
+        assertThrows(NoEqualsPasswordException.class, () -> contactService.changePassword(request, contact));
+    }
+}
---
+++ src/test/java/com/chat/yourway/service/impl/ContactServiceImplUnitTest.java
@@ -0,0 +1,76 @@
+package com.chat.yourway.service.impl;
+
+import com.chat.yourway.dto.request.ChangePasswordDto;
+import com.chat.yourway.exception.NoEqualsPasswordException;
+import com.chat.yourway.model.Contact;
+import com.chat.yourway.repository.ContactRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+public class ContactServiceImplUnitTest {
+    private Contact contact;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private ContactRepository contactRepository;
+
+    @InjectMocks
+    private ContactServiceImpl contactService;
+
+    @BeforeEach
+    public void setUp() {
+        contact = Contact.builder()
+                .id(1)
+                .username("username12353")
+                .email("user@gmail.com")
+                .password("encodedPassword")
+                .isActive(true)
+                .isPrivate(true)
+                .build();
+    }
+
+    @Test
+    public void testChangePassword_Success() {
+        String oldPassword = "oldPassword";
+        String newPassword = "newPassword";
+        String encodedPassword = contact.getPassword();
+        ChangePasswordDto request = new ChangePasswordDto(oldPassword, newPassword);
+
+
+        when(passwordEncoder.matches(oldPassword, encodedPassword)).thenReturn(true);
+        when(passwordEncoder.encode(newPassword)).thenReturn(encodedPassword);
+
+        contactService.changePassword(request, contact);
+
+        verify(passwordEncoder, times(1)).matches(oldPassword, encodedPassword);
+        verify(passwordEncoder, times(1)).encode(newPassword);
+        verify(contactRepository, times(1))
+                .changePasswordByUsername(encodedPassword, contact.getUsername());
+    }
+
+    @Test
+    public void testChangePassword_InvalidOldPassword() {
+        String oldPassword = "oldPassword";
+        String newPassword = "newPassword";
+        String encodedPassword = contact.getPassword();
+        ChangePasswordDto request = new ChangePasswordDto(oldPassword, newPassword);
+
+        when(passwordEncoder.matches(oldPassword, encodedPassword)).thenReturn(false);
+
+        assertThrows(NoEqualsPasswordException.class, () -> contactService.changePassword(request, contact));
+
+        verify(passwordEncoder, times(1)).matches(oldPassword, encodedPassword);
+        verify(passwordEncoder, never()).encode(anyString());
+        verify(contactRepository, never()).changePasswordByUsername(anyString(), anyString());
+    }
+}
