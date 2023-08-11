3576652582693fa09fcf808474d4ecfd6dd6a578
dmytro <dimastel7@gmail.com>
2023-08-11 11:10:17 +0300

    refactor: add producer/consumer type of value in endpoint for changing password

--- src/main/java/com/chat/yourway/controller/ContactController.java
+++ src/main/java/com/chat/yourway/controller/ContactController.java
@@ -10,17 +10,21 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Tag(name = "Contact")
 @RestController
 @RequestMapping("/contacts")
 @RequiredArgsConstructor
-@Tag(name = "Contact")
 public class ContactController {
 
     private final ContactService contactService;
 
-    @PatchMapping("/password")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     @Operation(summary = "Change to new password")
+    @PatchMapping(path = "/password",
+            produces = APPLICATION_JSON_VALUE,
+            consumes = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void changePassword(@RequestBody ChangePasswordDto request, @AuthenticationPrincipal UserDetails userDetails) {
         contactService.changePassword(request, userDetails);
     }
