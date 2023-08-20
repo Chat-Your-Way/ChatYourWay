528239c51a6d5a7ccd6bbf4066ab9e602dd21144
Dmytro Trotsenko <trohae7@gmail.com>
2023-08-20 10:52:47 +0300

    fix: removed {} at @ExceptionHandler

--- src/main/java/com/chat/yourway/exception/ApiExceptionHandler.java
+++ src/main/java/com/chat/yourway/exception/ApiExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ApiExceptionHandler {
 
-  @ExceptionHandler({ServiceException.class})
+  @ExceptionHandler(ServiceException.class)
   public ResponseEntity<ApiErrorResponse> handleNotFoundException(ServiceException exception) {
     return ResponseEntity.status(exception.getHttpStatus())
         .body(new ApiErrorResponse(exception.getHttpStatus(), exception.getMessage()));
