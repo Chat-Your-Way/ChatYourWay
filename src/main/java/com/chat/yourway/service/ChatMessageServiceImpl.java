4525b0cfc69b17727fdda249590f2eaaf6c0d5e9
Dmytro Trotsenko <trohae7@gmail.com>
2023-08-20 22:09:56 +0300

    fix: add clear class constructor

--- src/main/java/com/chat/yourway/service/ChatMessageServiceImpl.java
+++ src/main/java/com/chat/yourway/service/ChatMessageServiceImpl.java
@@ -4,7 +4,6 @@ import com.chat.yourway.dto.request.ReceivedMessageDto;
 import com.chat.yourway.dto.response.MessageResponseDto;
 import com.chat.yourway.mapper.MessageMapper;
 import com.chat.yourway.service.interfaces.ChatMessageService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -13,14 +12,19 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class ChatMessageServiceImpl implements ChatMessageService {
 
   private final SimpMessagingTemplate simpMessagingTemplate;
   private final MessageMapper messageMapper;
-  @Value("${socket.specific-prefix}")
   private final String specificPrefix;
+
+  public ChatMessageServiceImpl(SimpMessagingTemplate simpMessagingTemplate,
+      MessageMapper messageMapper, @Value("${socket.specific-prefix}") String specificPrefix) {
+    this.simpMessagingTemplate = simpMessagingTemplate;
+    this.messageMapper = messageMapper;
+    this.specificPrefix = specificPrefix;
+  }
 
   @Override
   public MessageResponseDto sendToTopic(ReceivedMessageDto receivedMessageDto, String username) {
