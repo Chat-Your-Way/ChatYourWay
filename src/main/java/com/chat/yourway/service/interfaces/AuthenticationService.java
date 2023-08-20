dfd29ca12705b6536f1dd15dbdff35fe61af7629
Dmytro Trotsenko <trohae7@gmail.com>
2023-08-20 22:10:36 +0300

    refactor: added javadoc

--- src/main/java/com/chat/yourway/service/interfaces/AuthenticationService.java
+++ src/main/java/com/chat/yourway/service/interfaces/AuthenticationService.java
@@ -3,13 +3,34 @@ package com.chat.yourway.service.interfaces;
 import com.chat.yourway.dto.request.AuthRequestDto;
 import com.chat.yourway.dto.request.ContactRequestDto;
 import com.chat.yourway.dto.response.AuthResponseDto;
+import com.chat.yourway.exception.InvalidTokenException;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface AuthenticationService {
 
+  /**
+   * Registers a new contact and returns access and refresh tokens.
+   *
+   * @param contactRequestDto Contact registration details.
+   * @param request HttpServletRequest.
+   * @return {@link AuthResponseDto} containing access and refresh tokens.
+   */
   AuthResponseDto register(ContactRequestDto contactRequestDto, HttpServletRequest request);
 
+  /**
+   * Authenticates a contact's credentials and generates access and refresh tokens.
+   *
+   * @param authRequestDto Authentication details containing email and password.
+   * @return {@link AuthResponseDto} containing access and refresh tokens.
+   */
   AuthResponseDto authenticate(AuthRequestDto authRequestDto);
 
+  /**
+   * Refreshes an access token using a refresh token.
+   *
+   * @param request HttpServletRequest.
+   * @return {@link AuthResponseDto} containing access and refresh tokens.
+   * @throws InvalidTokenException If the refresh token is invalid.
+   */
   AuthResponseDto refreshToken(HttpServletRequest request);
 }
--- src/main/java/com/chat/yourway/service/interfaces/ContactService.java
+++ src/main/java/com/chat/yourway/service/interfaces/ContactService.java
@@ -1,11 +1,27 @@
 package com.chat.yourway.service.interfaces;
 
 import com.chat.yourway.dto.request.ContactRequestDto;
+import com.chat.yourway.exception.ValueNotUniqException;
 import com.chat.yourway.model.Contact;
+import jakarta.persistence.EntityNotFoundException;
 
 public interface ContactService {
 
+  /**
+   * Creates a new contact.
+   *
+   * @param contactRequestDto Contact request details.
+   * @return {@link Contact} entity.
+   * @throws ValueNotUniqException If the provided email or username is already in use.
+   */
   Contact create(ContactRequestDto contactRequestDto);
 
+  /**
+   * Finds a contact entity by email.
+   *
+   * @param email Email of the contact.
+   * @return {@link Contact} entity.
+   * @throws EntityNotFoundException If contact by email wasn't found.
+   */
   Contact findByEmail(String email);
 }
