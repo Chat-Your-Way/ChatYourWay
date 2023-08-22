ebbf00fffc82cf61a0ea7c9ef598d7be95f3871e
Dmytro Trotsenko <trohae7@gmail.com>
2023-08-22 20:28:17 +0300

    feat: added CORS configuration

--- src/main/java/com/chat/yourway/config/security/SecurityFilterConfig.java
+++ src/main/java/com/chat/yourway/config/security/SecurityFilterConfig.java
@@ -1,7 +1,9 @@
 package com.chat.yourway.config.security;
 
 import com.chat.yourway.security.JwtAuthFilter;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -14,6 +16,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +28,15 @@ public class SecurityFilterConfig {
   private final JwtAuthFilter jwtAuthFilter;
   private final AuthenticationProvider authenticationProvider;
   private final LogoutHandler logoutHandler;
+
+  @Value("${cors.allowed-origins}")
+  private List<String> allowedOrigins;
+  @Value("${cors.allowed-methods}")
+  private List<String> allowedMethods;
+  @Value("${cors.allowed-headers}")
+  private List<String> allowedHeaders;
+  @Value("${cors.allow-credentials}")
+  private Boolean allowCredentials;
 
   private static final String[] WHITELIST_URLS = {
       //OpenApi
@@ -44,6 +58,7 @@ public class SecurityFilterConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
+        .cors(cors -> corsConfigurationSource())
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(WHITELIST_URLS).permitAll()
@@ -62,6 +77,18 @@ public class SecurityFilterConfig {
                 (request, response, authentication) -> SecurityContextHolder.clearContext())
         )
         .build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(allowedOrigins);
+    configuration.setAllowedMethods(allowedMethods);
+    configuration.setAllowedHeaders(allowedHeaders);
+    configuration.setAllowCredentials(allowCredentials);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
 }
