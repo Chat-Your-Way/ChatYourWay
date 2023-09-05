45702f5c46cf9d3323b35e6d377fb81aacb36e95
dmytro <dimastel7@gmail.com>
2023-09-05 22:07:44 +0300

    refactor: change annotations in AuthRequestDto

--- src/main/java/com/chat/yourway/dto/request/AuthRequestDto.java
+++ src/main/java/com/chat/yourway/dto/request/AuthRequestDto.java
@@ -1,8 +1,7 @@
 package com.chat.yourway.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import com.chat.yourway.annotation.EmailValidation;
+import com.chat.yourway.annotation.PasswordValidation;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,15 +16,9 @@ import lombok.ToString;
 @Builder
 @ToString
 public class AuthRequestDto {
-  @NotBlank(message = "The email should not be blank")
-  @Size(min = 6, max = 320, message = "The email must be at least 6 characters and not be longer than 320 characters")
-  @Pattern(regexp = "[a-z0-9.\\-_]+@[a-z]+\\.[a-z]{2,3}", message = "The email is invalid")
+  @EmailValidation
   private String email;
 
-  @NotBlank(message = "The password should not be blank")
-  @Size(min = 4, max = 12, message = "The password must be at least 4 characters and not be longer than 12 characters")
-  @Pattern(regexp = ".*[.,\\\\-_+&!;:'#*?].*", message = "Password must include at least 1 special symbol: . , - _ + & ! ; : ' # * ?")
-  @Pattern(regexp = ".*[A-Z].*", message = "Password must include at least 1 Upper-case letter")
-  @Pattern(regexp = ".*\\d.*", message = "Password must include at least 1 digit")
+  @PasswordValidation
   private String password;
 }
