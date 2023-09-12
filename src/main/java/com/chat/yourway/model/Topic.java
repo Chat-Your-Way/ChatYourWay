d8917de216dbe3b2f3376bbdf6b23e8456055b67
Dmytro Trotsenko <trohae7@gmail.com>
2023-09-12 22:17:39 +0300

    feat: added Topic and TopicSubscriber entity

---
+++ src/main/java/com/chat/yourway/model/Topic.java
@@ -0,0 +1,47 @@
+package com.chat.yourway.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(schema = "chat", name = "topic")
+public class Topic {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "topic_seq_gen")
+  @SequenceGenerator(name = "topic_seq_gen", sequenceName = "chat.topic_id_seq", allocationSize = 1)
+  private Integer id;
+
+  @Column(name = "topic_name", nullable = false, unique = true)
+  private String topicName;
+
+  @Column(name = "created_by", nullable = false)
+  private String createdBy;
+
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt = LocalDateTime.now();
+
+  @OneToMany(mappedBy = "topic", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  private Set<TopicSubscriber> topicSubscribers;
+
+}
