564b1057638fefe153bb036ea4c3d06d6a487c80
Dmytro Trotsenko <trohae7@gmail.com>
2023-09-17 11:07:29 +0300

    refactor: added responseDto and refactored

--- src/main/java/com/chat/yourway/controller/ContactController.java
+++ src/main/java/com/chat/yourway/controller/ContactController.java
@@ -1,10 +1,14 @@
 package com.chat.yourway.controller;
 
+import static com.chat.yourway.config.openapi.OpenApiMessages.CONTACT_NOT_FOUND;
+import static com.chat.yourway.config.openapi.OpenApiMessages.CONTACT_UNAUTHORIZED;
+import static com.chat.yourway.config.openapi.OpenApiMessages.SUCCESSFULLY_UPDATED_CONTACT_PROFILE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 import com.chat.yourway.config.openapi.OpenApiExamples;
 import com.chat.yourway.dto.request.EditContactProfileRequestDto;
 import com.chat.yourway.dto.response.ApiErrorResponseDto;
 import com.chat.yourway.service.interfaces.ContactService;
-import com.chat.yourway.service.interfaces.TopicSubscriberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -12,49 +16,39 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-
-import static com.chat.yourway.config.openapi.OpenApiMessages.*;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Contact")
 @RestController
 @RequestMapping("/contacts")
 @RequiredArgsConstructor
 public class ContactController {
-    private final ContactService contactService;
-    private final TopicSubscriberService topicSubscriberService;
 
-    @Operation(summary = "Registration a new contact",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = SUCCESSFULLY_UPDATED_CONTACT_PROFILE),
-                    @ApiResponse(responseCode = "404", description = CONTACT_NOT_FOUND,
-                            content = @Content(schema = @Schema(implementation = ApiErrorResponseDto.class)))
-            },
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    content = @Content(schema = @Schema(implementation = EditContactProfileRequestDto.class),
-                            examples = @ExampleObject(value = OpenApiExamples.EDIT_CONTACT_PROFILE,
-                                    description = "Edit Contact profile"))))
-    @PatchMapping(path = "/profile", consumes = APPLICATION_JSON_VALUE)
-    public void editContactProfile(@Valid @RequestBody EditContactProfileRequestDto editContactProfileRequestDto,
-                                    @AuthenticationPrincipal UserDetails userDetails) {
-        contactService.updateContactProfile(editContactProfileRequestDto, userDetails);
-    }
+  private final ContactService contactService;
 
-    @PostMapping(path = "/subscribe/{topicId}")
-    public void subscribeToTopic(@PathVariable Integer topicId, Principal principal) {
-        String email = principal.getName();
-        topicSubscriberService.subscribeToTopic(email, topicId);
-    }
-
-    @PostMapping(path = "/unsubscribe/{topicId}")
-    public void unsubscribeFromTopic(@PathVariable Integer topicId, Principal principal){
-        String email = principal.getName();
-        topicSubscriberService.unsubscribeFromTopic(email, topicId);
-    }
+  @Operation(summary = "Edit contact profile",
+      responses = {
+          @ApiResponse(responseCode = "200", description = SUCCESSFULLY_UPDATED_CONTACT_PROFILE),
+          @ApiResponse(responseCode = "404", description = CONTACT_NOT_FOUND,
+              content = @Content(schema = @Schema(implementation = ApiErrorResponseDto.class))),
+          @ApiResponse(responseCode = "403", description = CONTACT_UNAUTHORIZED,
+              content = @Content(schema = @Schema(implementation = ApiErrorResponseDto.class)))
+      },
+      requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+          content = @Content(schema = @Schema(implementation = EditContactProfileRequestDto.class),
+              examples = @ExampleObject(value = OpenApiExamples.EDIT_CONTACT_PROFILE,
+                  description = "Edit Contact profile"))))
+  @PatchMapping(path = "/profile", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+  public void editContactProfile(
+      @Valid @RequestBody EditContactProfileRequestDto editContactProfileRequestDto,
+      @AuthenticationPrincipal UserDetails userDetails) {
+    contactService.updateContactProfile(editContactProfileRequestDto, userDetails);
+  }
 
 }
--- src/main/java/com/chat/yourway/exception/OldPasswordsIsNotEqualToNewException.java
+++
@@ -1,8 +0,0 @@
-package com.chat.yourway.exception;
-
-public class OldPasswordsIsNotEqualToNewException extends RuntimeException {
-
-  public OldPasswordsIsNotEqualToNewException(String message) {
-    super(message);
-  }
-}
