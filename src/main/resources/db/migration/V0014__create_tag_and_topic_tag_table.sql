1b2d1f85cd646487972840607af6b4f57f92c25d
dmytro <dimastel7@gmail.com>
2023-09-18 14:13:59 +0300

    refactor: rename constraints in table `topic_tag`

--- src/main/resources/db/migration/V0014__create_tag_and_topic_tag_table.sql
+++ src/main/resources/db/migration/V0014__create_tag_and_topic_tag_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS chat.tag
 create table if not exists chat.topic_tag(
     topic_id            INTEGER        NOT NULL,
     tag_id            INTEGER        NOT NULL,
-    CONSTRAINT topic_id_tag_id_pkey PRIMARY KEY (topic_id, tag_id),
-    CONSTRAINT topic_tag_topic_topic_id_fkey FOREIGN KEY (topic_id) REFERENCES chat.topic (id),
-    CONSTRAINT topic_tag_tag_tag_id_fkey FOREIGN KEY (tag_id) REFERENCES chat.tag (id)
+    CONSTRAINT topic_tag_id_pkey PRIMARY KEY (topic_id, tag_id),
+    CONSTRAINT topic_tag_topic_id_fkey FOREIGN KEY (topic_id) REFERENCES chat.topic (id),
+    CONSTRAINT topic_tag_tag_id_fkey FOREIGN KEY (tag_id) REFERENCES chat.tag (id)
     );
