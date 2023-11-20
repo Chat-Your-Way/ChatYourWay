3be42688b9d64ac3068aa2f2dd4998c380e7d484
dmytro <dimastel7@gmail.com>
2023-11-20 23:38:16 +0200

    test: prepare dataset for testing permission of sending private messages

--- src/test/java/com/chat/yourway/integration/service/impl/TopicSubscriberServiceImplTest.java
+++ src/test/java/com/chat/yourway/integration/service/impl/TopicSubscriberServiceImplTest.java
@@ -1,9 +1,11 @@
 package com.chat.yourway.integration.service.impl;
 
+import com.chat.yourway.dto.response.TopicSubscriberResponseDto;
 import com.chat.yourway.exception.NotSubscribedTopicException;
 import com.chat.yourway.exception.TopicNotFoundException;
 import com.chat.yourway.integration.extension.PostgresExtension;
 import com.chat.yourway.integration.extension.RedisExtension;
+import com.chat.yourway.model.TopicSubscriber;
 import com.chat.yourway.service.interfaces.ContactService;
 import com.chat.yourway.service.interfaces.TopicService;
 import com.chat.yourway.service.interfaces.TopicSubscriberService;
@@ -51,6 +53,7 @@ public class TopicSubscriberServiceImplTest {
       type = DatabaseOperation.DELETE)
   @DisplayName("should successfully add topic to favourite when user mark topic as favourite")
   public void shouldSuccessfullyAddTopicToFavourite_whenUserMarkTopicAsFavourite() {
+    // Given
     var contactEmail = "vasil1@gmail.com";
     var contact = contactService.findByEmail(contactEmail);
     var expectedSize = 2;
@@ -76,6 +79,7 @@ public class TopicSubscriberServiceImplTest {
   @DisplayName(
       "should successfully remove topic from favourite when user unmark topic as favourite")
   public void shouldSuccessfullyRemoveTopicFromFavourite_whenUserUnmarkTopicAsFavourite() {
+    // Given
     var contactEmail = "vasil1@gmail.com";
     var contact = contactService.findByEmail(contactEmail);
     var expectedSize = 0;
@@ -101,6 +105,7 @@ public class TopicSubscriberServiceImplTest {
   @DisplayName(
       "should throw TopicNotFoundException when user mark topic as favourite and topic does not exist")
   public void shouldThrowTopicNotFoundException_whenUserMarkTopicAsFavouriteAndTopicDoesNotExist() {
+    // Given
     var contactEmail = "vasil1@gmail.com";
     var contact = contactService.findByEmail(contactEmail);
     var topicId = 1;
@@ -123,6 +128,7 @@ public class TopicSubscriberServiceImplTest {
       "should throw TopicNotFoundException when user unmark topic as favourite and topic does not exist")
   public void
       shouldThrowTopicNotFoundException_whenUserUnmarkTopicAsFavouriteAndTopicDoesNotExist() {
+    // Given
     var contactEmail = "vasil1@gmail.com";
     var contact = contactService.findByEmail(contactEmail);
     var topicId = 1;
@@ -145,6 +151,7 @@ public class TopicSubscriberServiceImplTest {
       "should throw NotSubscribedTopicException when user mark topic as favourite and user did not subscribe to topic")
   public void
       shouldThrowNotSubscribedTopicException_whenUserMarkTopicAsFavouriteAndUserDidNotSubscribeToTopic() {
+    // Given
     var contactEmail = "vasil1@gmail.com";
     var contact = contactService.findByEmail(contactEmail);
     var topicId = 113;
@@ -167,6 +174,7 @@ public class TopicSubscriberServiceImplTest {
       "should throw NotSubscribedTopicException when user unmark topic as favourite and user did not subscribe to topic")
   public void
       shouldThrowNotSubscribedTopicException_whenUserUnmarkTopicAsFavouriteAndUserDidNotSubscribeToTopic() {
+    // Given
     var contactEmail = "vasil1@gmail.com";
     var contact = contactService.findByEmail(contactEmail);
     var topicId = 113;
@@ -176,5 +184,129 @@ public class TopicSubscriberServiceImplTest {
     assertThrows(
         NotSubscribedTopicException.class,
         () -> topicSubscriberService.removeTopicFromFavourite(topicId, contact));
+  }
+
+  @Test
+  @DatabaseSetup(
+      value = "/dataset/permission-sending-message-in-private-topic.xml",
+      type = DatabaseOperation.INSERT)
+  @DatabaseTearDown(
+      value = "/dataset/permission-sending-message-in-private-topic.xml",
+      type = DatabaseOperation.DELETE)
+  @DisplayName(
+      "should successfully permit sending private messages when user change permission of topic")
+  public void shouldSuccessfullyPermitSendingPrivateMessages_whenUserChangePermissionOfTopic() {
+    // Given
+    var contactEmail = "vasil299@gmail.com";
+    var contact = contactService.findByEmail(contactEmail);
+    var topicId = 212;
+
+    // When
+    topicSubscriberService.permitSendingPrivateMessages(topicId, contact);
+
+    // Then
+    var result =
+        topicService.findById(topicId).getTopicSubscribers().stream()
+            .filter(ts -> ts.getContact().getEmail().equals(contactEmail))
+            .findFirst()
+            .orElseThrow(() -> new AssertionError("Expected subscriber of topic is not present."));
+
+    assertThat(result)
+        .extracting(TopicSubscriberResponseDto::isPermittedSendingMessage)
+        .isEqualTo(true);
+  }
+
+  @Test
+  @DatabaseSetup(
+      value = "/dataset/permission-sending-message-in-private-topic.xml",
+      type = DatabaseOperation.INSERT)
+  @DatabaseTearDown(
+      value = "/dataset/permission-sending-message-in-private-topic.xml",
+      type = DatabaseOperation.DELETE)
+  @DisplayName(
+      "should successfully prohibit sending private messages when user change permission of topic")
+  public void shouldSuccessfullyProhibitSendingPrivateMessages_whenUserChangePermissionOfTopic() {
+    // Given
+    var contactEmail = "vasil299@gmail.com";
+    var contact = contactService.findByEmail(contactEmail);
+    var topicId = 211;
+
+    // When
+    topicSubscriberService.prohibitSendingPrivateMessages(topicId, contact);
+
+    // Then
+    var result =
+        topicService.findById(topicId).getTopicSubscribers().stream()
+            .filter(ts -> ts.getContact().getEmail().equals(contactEmail))
+            .findFirst()
+            .orElseThrow(() -> new AssertionError("Expected subscriber of topic is not present."));
+
+    assertThat(result)
+        .extracting(TopicSubscriberResponseDto::isPermittedSendingMessage)
+        .isEqualTo(false);
+  }
+
+  @Test
+  @DatabaseSetup(
+      value = "/dataset/permission-sending-message-in-private-topic.xml",
+      type = DatabaseOperation.INSERT)
+  @DatabaseTearDown(
+      value = "/dataset/permission-sending-message-in-private-topic.xml",
+      type = DatabaseOperation.DELETE)
+  @DisplayName(
+      "should throw TopicNotFoundException when user change permission of not existed topic")
+  public void shouldThrowTopicNotFoundException_whenUserChangePermissionOfNotExistedTopic() {
+    // Given
+    var contactEmail = "vasil299@gmail.com";
+    var contact = contactService.findByEmail(contactEmail);
+    var topicId = 217;
+
+    // When
+    // Then
+    assertThrows(
+        TopicNotFoundException.class,
+        () -> topicSubscriberService.permitSendingPrivateMessages(topicId, contact));
+  }
+
+  @Test
+  @DatabaseSetup(
+      value = "/dataset/permission-sending-message-in-private-topic.xml",
+      type = DatabaseOperation.INSERT)
+  @DatabaseTearDown(
+      value = "/dataset/permission-sending-message-in-private-topic.xml",
+      type = DatabaseOperation.DELETE)
+  @DisplayName("should throw TopicNotFoundException when user change permission of public topic")
+  public void shouldThrowTopicNotFoundException_whenUserChangePermissionOfPublicTopic() {
+    // Given
+    var contactEmail = "vasil299@gmail.com";
+    var contact = contactService.findByEmail(contactEmail);
+    var topicId = 213;
+
+    // When
+    // Then
+    assertThrows(
+        TopicNotFoundException.class,
+        () -> topicSubscriberService.permitSendingPrivateMessages(topicId, contact));
+  }
+
+  @Test
+  @DatabaseSetup(
+      value = "/dataset/permission-sending-message-in-private-topic.xml",
+      type = DatabaseOperation.INSERT)
+  @DatabaseTearDown(
+      value = "/dataset/permission-sending-message-in-private-topic.xml",
+      type = DatabaseOperation.DELETE)
+  @DisplayName("should throw NotSubscribedException when user change permission of topic")
+  public void shouldThrowNotSubscribedException_whenUserChangePermissionOfTopic() {
+    // Given
+    var contactEmail = "anton912@gmail.com";
+    var contact = contactService.findByEmail(contactEmail);
+    var topicId = 212;
+
+    // When
+    // Then
+    assertThrows(
+        NotSubscribedTopicException.class,
+        () -> topicSubscriberService.permitSendingPrivateMessages(topicId, contact));
   }
 }
