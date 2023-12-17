2a0dcdf36d540c0dcb53f59b7f579b920bb2a246
Dmytro Trotsenko <trohae7@gmail.com>
2023-12-18 00:39:06 +0200

    bugfix: fixed save contact to repository

--- src/main/java/com/chat/yourway/service/ActivateAccountServiceImpl.java
+++ src/main/java/com/chat/yourway/service/ActivateAccountServiceImpl.java
@@ -6,6 +6,7 @@ import com.chat.yourway.dto.common.EmailMessageInfoDto;
 import com.chat.yourway.exception.EmailTokenNotFoundException;
 import com.chat.yourway.model.Contact;
 import com.chat.yourway.model.email.EmailToken;
+import com.chat.yourway.repository.ContactRepository;
 import com.chat.yourway.repository.EmailTokenRepository;
 import com.chat.yourway.service.interfaces.ActivateAccountService;
 import com.chat.yourway.service.interfaces.EmailMessageFactoryService;
@@ -23,21 +24,33 @@ public class ActivateAccountServiceImpl implements ActivateAccountService {
   private final EmailSenderService emailSenderService;
   private final EmailTokenRepository emailTokenRepository;
   private final EmailMessageFactoryService emailMessageFactoryService;
+  private final ContactRepository contactRepository;
 
   @Transactional
   @Override
   public void activateAccount(String token) {
+    log.trace("Started activateAccount by email token");
+
     EmailToken emailToken = emailTokenRepository.findById(token)
-        .orElseThrow(EmailTokenNotFoundException::new);
+        .orElseThrow(() -> {
+          log.warn("Current email token does not exist in repository");
+          return new EmailTokenNotFoundException();
+        });
 
     Contact contact = emailToken.getContact();
-
     contact.setIsActive(true);
+    contactRepository.save(contact);
+
     emailTokenRepository.delete(emailToken);
+
+    log.info("Account is activate for contact email [{}]", contact.getEmail());
   }
 
   @Override
   public void sendVerifyEmail(Contact contact, String clientHost) {
+    log.trace("Started sendVerifyEmail by contact email [{}], and client host [{}]",
+        contact.getEmail(), clientHost);
+
     String uuid = generateUUID();
     saveEmailToken(contact, uuid);
 
@@ -46,7 +59,8 @@ public class ActivateAccountServiceImpl implements ActivateAccountService {
     var emailMessage = emailMessageFactoryService.generateEmailMessage(emailMessageInfoDto);
 
     emailSenderService.sendEmail(emailMessage);
-    log.info("Email for verifying account sent");
+
+    log.info("Verifying account email was sent to contact email [{}]", contact.getEmail());
   }
 
   private void saveEmailToken(Contact contact, String uuid) {
