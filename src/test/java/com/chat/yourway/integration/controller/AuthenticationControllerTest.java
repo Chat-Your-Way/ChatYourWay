177729b732d644aca7729e5d5bd5ade08dceb2ea
Dmytro Trotsenko <trohae7@gmail.com>
2023-12-18 00:44:07 +0200

    feat: added integration tests to AuthenticationControllerTest

--- src/test/java/com/chat/yourway/integration/controller/AuthenticationControllerTest.java
+++ src/test/java/com/chat/yourway/integration/controller/AuthenticationControllerTest.java
@@ -1,16 +1,24 @@
 package com.chat.yourway.integration.controller;
 
+import static com.github.springtestdbunit.annotation.DatabaseOperation.CLEAN_INSERT;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.chat.yourway.dto.request.AuthRequestDto;
 import com.chat.yourway.dto.request.ContactRequestDto;
+import com.chat.yourway.exception.InvalidCredentialsException;
 import com.chat.yourway.integration.extension.PostgresExtension;
 import com.chat.yourway.integration.extension.RedisExtension;
 import com.chat.yourway.security.JwtService;
-import com.chat.yourway.security.TokenService;
 import com.chat.yourway.service.interfaces.ActivateAccountService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.DatabaseTearDown;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,50 +30,51 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
 import org.springframework.boot.test.mock.mockito.ResetMocksTestExecutionListener;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith({PostgresExtension.class, RedisExtension.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc(addFilters = false)
 @TestExecutionListeners(
     value = {
-      TransactionalTestExecutionListener.class,
-      DirtiesContextTestExecutionListener.class,
-      DependencyInjectionTestExecutionListener.class,
-      DbUnitTestExecutionListener.class,
-      MockitoTestExecutionListener.class,
-      ResetMocksTestExecutionListener.class
+        DependencyInjectionTestExecutionListener.class,
+        DbUnitTestExecutionListener.class,
+        MockitoTestExecutionListener.class,
+        ResetMocksTestExecutionListener.class
     })
+@DatabaseSetup(value = "/dataset/mockdb/contact.xml", type = CLEAN_INSERT)
 public class AuthenticationControllerTest {
+
   private static final String URI = "/auth";
   private static final String REFERER = "https://referrer.com";
+  private static final String TOKEN_TYPE = "Bearer ";
+  public static final String EMAIL_TOKEN = "24596e34-cc65-4dce-b374-7419fbfc18e5";
 
-  @MockBean private JwtService jwtService;
-  @MockBean private TokenService tokenService;
-  @MockBean private ActivateAccountService activateAccountService;
-  @MockBean private AuthenticationManager authManager;
-  @Autowired MockMvc mockMvc;
-  @Autowired private ObjectMapper objectMapper;
+  @MockBean
+  private AuthenticationManager authManager;
+
+  @Autowired
+  private JwtService jwtService;
+  @Autowired
+  private ActivateAccountService activateAccountService;
+  @Autowired
+  MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
 
   @Test
   @SneakyThrows
-  @DisplayName("should register new contact when user inputted existed nickname")
-  @DatabaseSetup(value = "/dataset/contacts.xml", type = DatabaseOperation.INSERT)
-  @DatabaseTearDown(value = "/dataset/contacts.xml", type = DatabaseOperation.DELETE)
-  public void shouldRegisterNewContact_whenUserInputtedExistedNickname() {
+  @DisplayName("register should register new contact when user inputted existed nickname")
+  public void register_shouldRegisterNewContact_whenUserInputtedExistedNickname() {
     // Given
-    var nickname = "username12345";
-    var password = "User12346*";
+    var nickname = "NewUser";
+    var password = "Password-123";
     var avatarId = (byte) 1;
-    var request = new ContactRequestDto(nickname, "user2@gmail.com", avatarId, password);
+    var request = new ContactRequestDto(nickname, "newuser@gmail.com", avatarId, password);
 
     // When
     var response =
@@ -76,20 +85,20 @@ public class AuthenticationControllerTest {
                 .contentType(APPLICATION_JSON));
 
     // Then
-    response.andExpect(status().isCreated()).andExpect(content().contentType(APPLICATION_JSON));
+    response
+        .andExpect(status().isCreated())
+        .andExpect(content().contentType(APPLICATION_JSON));
   }
 
   @Test
   @SneakyThrows
-  @DisplayName("should return status code 409 when user inputted existed email")
-  @DatabaseSetup(value = "/dataset/contacts.xml", type = DatabaseOperation.INSERT)
-  @DatabaseTearDown(value = "/dataset/contacts.xml", type = DatabaseOperation.DELETE)
-  public void shouldReturnStatusCode409_whenUserInputtedExistedEmail() {
+  @DisplayName("register should return status code 409 when user inputted existed email")
+  public void register_shouldReturnStatusCode409_whenUserInputtedExistedEmail() {
     // Given
-    var nickname = "username12345";
-    var password = "User12346*";
+    var nickname = "NewUser";
+    var password = "Password-123";
     var avatarId = (byte) 1;
-    var request = new ContactRequestDto(nickname, "user@gmail.com", avatarId, password);
+    var request = new ContactRequestDto(nickname, "vasil@gmail.com", avatarId, password);
 
     // When
     var response =
@@ -100,18 +109,18 @@ public class AuthenticationControllerTest {
                 .contentType(APPLICATION_JSON));
 
     // Then
-    response.andExpect(status().isConflict()).andExpect(content().contentType(APPLICATION_JSON));
+    response
+        .andExpect(status().isConflict())
+        .andExpect(content().contentType(APPLICATION_JSON));
   }
 
   @Test
   @SneakyThrows
-  @DisplayName("should register new contact when user inputted uppercase email")
-  @DatabaseSetup(value = "/dataset/contacts.xml", type = DatabaseOperation.INSERT)
-  @DatabaseTearDown(value = "/dataset/contacts.xml", type = DatabaseOperation.DELETE)
-  public void shouldRegisterNewContact_whenUserInputtedUppercaseEmail() {
+  @DisplayName("register should register new contact when user inputted uppercase email")
+  public void register_shouldRegisterNewContact_whenUserInputtedUppercaseEmail() {
     // Given
-    var nickname = "username12345";
-    var password = "User12346*";
+    var nickname = "NewUser";
+    var password = "Password-123";
     var avatarId = (byte) 1;
     var request = new ContactRequestDto(nickname, "NEWEMAIL54@GMAIL.COM", avatarId, password);
 
@@ -124,20 +133,20 @@ public class AuthenticationControllerTest {
                 .contentType(APPLICATION_JSON));
 
     // Then
-    response.andExpect(status().isCreated()).andExpect(content().contentType(APPLICATION_JSON));
+    response
+        .andExpect(status().isCreated())
+        .andExpect(content().contentType(APPLICATION_JSON));
   }
 
   @Test
   @SneakyThrows
-  @DisplayName("should return status code 409 when user inputted existed uppercase email")
-  @DatabaseSetup(value = "/dataset/contacts.xml", type = DatabaseOperation.INSERT)
-  @DatabaseTearDown(value = "/dataset/contacts.xml", type = DatabaseOperation.DELETE)
-  public void shouldReturnStatusCode409_whenUserInputtedExistedUppercaseEmail() {
+  @DisplayName("register should return status code 409 when user inputted existed uppercase email")
+  public void register_shouldReturnStatusCode409_whenUserInputtedExistedUppercaseEmail() {
     // Given
-    var nickname = "username12345";
-    var password = "User12346*";
+    var nickname = "NewUser";
+    var password = "Password-123";
     var avatarId = (byte) 1;
-    var request = new ContactRequestDto(nickname, "NEWEMAIL@GMAIL.COM", avatarId, password);
+    var request = new ContactRequestDto(nickname, "VASIL@GMAIL.COM", avatarId, password);
 
     // When
     var response =
@@ -148,6 +157,225 @@ public class AuthenticationControllerTest {
                 .contentType(APPLICATION_JSON));
 
     // Then
-    response.andExpect(status().isConflict()).andExpect(content().contentType(APPLICATION_JSON));
+    response
+        .andExpect(status().isConflict())
+        .andExpect(content().contentType(APPLICATION_JSON));
   }
+
+
+  @Test
+  @SneakyThrows
+  @DisplayName("login should authenticate user with valid credentials")
+  void login_shouldAuthenticateUserWithValidCredentials() {
+    // Given
+    AuthRequestDto authRequestDto = AuthRequestDto.builder()
+        .email("vasil@gmail.com")
+        .password("Password-123")
+        .build();
+
+    // When
+    var response = mockMvc.perform(
+        post(URI + "/login")
+            .contentType(APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(authRequestDto))
+    );
+
+    // Then
+    response
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(APPLICATION_JSON))
+        .andExpect(jsonPath("$.accessToken").isString())
+        .andExpect(jsonPath("$.refreshToken").isString());
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("login should return status code 404 when email was not found")
+  void login_shouldReturnStatusCode404WhenEmailWasNotFound() {
+    // Given
+    AuthRequestDto authRequestDto = AuthRequestDto.builder()
+        .email("uncknown@gmail.com")
+        .password("Password-123")
+        .build();
+
+    // When
+    var response = mockMvc.perform(
+        post(URI + "/login")
+            .contentType(APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(authRequestDto))
+    );
+
+    // Then
+    response
+        .andExpect(status().isNotFound())
+        .andExpect(content().contentType(APPLICATION_JSON));
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("login should return status code 401 when wrong password")
+  void login_shouldReturnStatusCode401WhenWrongPassword() {
+    // Given
+    String email = "vasil@gmail.com";
+    String password = "Password-321";
+    AuthRequestDto authRequestDto = AuthRequestDto.builder()
+        .email(email)
+        .password(password)
+        .build();
+    when(authManager.authenticate(new UsernamePasswordAuthenticationToken(email, password)))
+        .thenThrow(InvalidCredentialsException.class);
+
+    // When
+    var response = mockMvc.perform(
+        post(URI + "/login")
+            .contentType(APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(authRequestDto))
+    );
+
+    // Then
+    response
+        .andExpect(status().isUnauthorized())
+        .andExpect(content().contentType(APPLICATION_JSON));
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("refresh should return new access token")
+  void refresh_shouldReturnNewAccessToken() {
+    // Given
+    String refreshToken = getRefreshToken();
+
+    // When
+    var response = mockMvc.perform(
+        post(URI + "/refresh")
+            .with(request -> {
+              request.addHeader(AUTHORIZATION, TOKEN_TYPE + refreshToken);
+              return request;
+            })
+            .contentType(APPLICATION_JSON)
+    );
+
+    // Then
+    response
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(APPLICATION_JSON))
+        .andExpect(jsonPath("$.accessToken").isString())
+        .andExpect(jsonPath("$.refreshToken").value(refreshToken));
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("refresh should return 401 when invalid refresh token")
+  void refresh_shouldReturn401WhenInvalidRefreshToken() {
+    // Given
+    String refreshToken = "invalidRefreshToken";
+
+    // When
+    var response = mockMvc.perform(
+        post(URI + "/refresh")
+            .with(request -> {
+              request.addHeader(AUTHORIZATION, TOKEN_TYPE + refreshToken);
+              return request;
+            })
+            .contentType(APPLICATION_JSON)
+    );
+
+    // Then
+    response
+        .andExpect(status().isUnauthorized())
+        .andExpect(content().contentType(APPLICATION_JSON));
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("activate should activate account successfully")
+  @DatabaseSetup(value = "/dataset/mockdb/email_token.xml", type = CLEAN_INSERT)
+  void activate_shouldActivateAccountSuccessfully() {
+    // When
+    var response = mockMvc.perform(
+        post(URI + "/activate")
+            .param("Email token", EMAIL_TOKEN)
+            .contentType(APPLICATION_JSON)
+    );
+
+    // Then
+    response
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("activate should return 404 when email token not found")
+  @DatabaseSetup(value = "/dataset/mockdb/email_token.xml", type = CLEAN_INSERT)
+  void activate_shouldReturn404WhenEmailTokenNotFound() {
+    // When
+    var response = mockMvc.perform(
+        post(URI + "/activate")
+            .param("Email token", "invalid_email_token")
+            .contentType(APPLICATION_JSON)
+    );
+
+    // Then
+    response
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("logout should successfully logged out")
+  void logout_shouldSuccessfullyLoggedOut() {
+    // Given
+    String accessToken = getAccessToken();
+
+    // When
+    var response = mockMvc.perform(
+        post(URI + "/logout")
+            .with(request -> {
+              request.addHeader(AUTHORIZATION, TOKEN_TYPE + accessToken);
+              return request;
+            })
+    );
+
+    // Then
+    response
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("logout should return 401 when invalid access token")
+  void logout_shouldReturn401WhenInvalidAccessToken() {
+    // Given
+    String accessToken = "invalidAccessToken";
+
+    // When
+    var response = mockMvc.perform(
+        post(URI + "/logout")
+            .with(request -> {
+              request.addHeader(AUTHORIZATION, TOKEN_TYPE + accessToken);
+              return request;
+            })
+    );
+
+    // Then
+    response
+        .andExpect(status().isUnauthorized());
+  }
+
+  private String getAccessToken() {
+    return jwtService.generateAccessToken(User
+        .withUsername("vasil@gmail.com")
+        .password("password")
+        .roles("USER")
+        .build());
+  }
+
+  private String getRefreshToken() {
+    return jwtService.generateRefreshToken(User
+        .withUsername("vasil@gmail.com")
+        .password("password")
+        .roles("USER")
+        .build());
+  }
+
 }
