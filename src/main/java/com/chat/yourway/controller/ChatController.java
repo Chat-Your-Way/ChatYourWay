b76d468f2b64647c3dff9e5d3603c9fe80eb7867
Dmytro Trotsenko <trohae7@gmail.com>
2024-01-16 00:03:31 +0200

    feat: added getTopicHistory

--- src/main/java/com/chat/yourway/controller/ChatController.java
+++ src/main/java/com/chat/yourway/controller/ChatController.java
@@ -2,10 +2,12 @@ package com.chat.yourway.controller;
 
 import com.chat.yourway.dto.request.MessagePrivateRequestDto;
 import com.chat.yourway.dto.request.MessagePublicRequestDto;
+import com.chat.yourway.dto.request.PageRequestDto;
 import com.chat.yourway.dto.response.MessageResponseDto;
 import com.chat.yourway.service.interfaces.ChatMessageService;
 import jakarta.validation.Valid;
 import java.security.Principal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -30,6 +32,13 @@ public class ChatController {
       @Valid @Payload MessagePrivateRequestDto message, Principal principal) {
     String email = principal.getName();
     return chatMessageService.sendToPrivateTopic(topicId, message, email);
+  }
+
+  @MessageMapping("/history/topic/{topicId}")
+  public List<MessageResponseDto> getTopicHistory(@DestinationVariable Integer topicId,
+      @Valid @Payload PageRequestDto pageRequestDto, Principal principal) {
+    String email = principal.getName();
+    return chatMessageService.sendMessageHistoryByTopicId(topicId, pageRequestDto, email);
   }
 
 }
