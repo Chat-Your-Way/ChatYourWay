2ccf684285489113a0c45ef3280e38d8c2c10875
Dmytro Trotsenko <trohae7@gmail.com>
2024-01-22 21:22:25 +0200

    fix: fix caching, added logging and refactoring

--- src/main/java/com/chat/yourway/service/ContactServiceImpl.java
+++ src/main/java/com/chat/yourway/service/ContactServiceImpl.java
@@ -3,13 +3,16 @@ package com.chat.yourway.service;
 import com.chat.yourway.dto.request.ContactRequestDto;
 import com.chat.yourway.dto.request.EditContactProfileRequestDto;
 import com.chat.yourway.dto.response.ContactProfileResponseDto;
-import com.chat.yourway.exception.*;
+import com.chat.yourway.exception.ContactNotFoundException;
+import com.chat.yourway.exception.PasswordsAreNotEqualException;
+import com.chat.yourway.exception.ValueNotUniqException;
 import com.chat.yourway.model.Contact;
 import com.chat.yourway.model.Role;
 import com.chat.yourway.repository.ContactRepository;
 import com.chat.yourway.service.interfaces.ContactService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,13 +30,15 @@ public class ContactServiceImpl implements ContactService {
   @Transactional
   @Override
   public Contact create(ContactRequestDto contactRequestDto) {
-    log.trace("Started create contact, contact email: {}", contactRequestDto.getEmail());
+    log.trace("Started create contact, contact email: [{}]", contactRequestDto.getEmail());
+
     if (isEmailExists(contactRequestDto.getEmail())) {
+      log.warn("Email [{}] already in use", contactRequestDto.getEmail());
       throw new ValueNotUniqException(
-          String.format("Email %s already in use", contactRequestDto.getEmail()));
+          String.format("Email [%s] already in use", contactRequestDto.getEmail()));
     }
 
-    return contactRepository.save(
+    Contact contact = contactRepository.save(
         Contact.builder()
             .nickname(contactRequestDto.getNickname())
             .avatarId(contactRequestDto.getAvatarId())
@@ -43,62 +48,78 @@ public class ContactServiceImpl implements ContactService {
             .isPrivate(true)
             .role(Role.USER)
             .build());
+
+    log.info("New contact with email [{}] was created", contactRequestDto.getEmail());
+    return contact;
   }
 
   @Override
-  @Cacheable("contacts")
+  @Transactional(readOnly = true)
+  @Cacheable(value = "contacts", key = "#email")
   public Contact findByEmail(String email) {
-    log.trace("Started findByEmail: {}", email);
-    return contactRepository
+    log.trace("Started findByEmail: [{}]", email);
+    Contact contact = contactRepository
         .findByEmailIgnoreCase(email)
-        .orElseThrow(
-            () -> new ContactNotFoundException(String.format("Email %s wasn't found", email)));
+        .orElseThrow(() -> {
+          log.warn("Email [{}] wasn't found", email);
+          return new ContactNotFoundException(String.format("Email [%s] wasn't found", email));
+        });
+
+    log.info("Contact was found by email [{}]", email);
+    return contact;
   }
 
   @Override
+  @Transactional
+  @CacheEvict(value = "contacts", key = "#email")
   public void changePasswordByEmail(String password, String email) {
+    log.trace("Started change password by email [{}]", email);
     contactRepository.changePasswordByEmail(passwordEncoder.encode(password), email);
+    log.info("Password was changed by email [{}]", email);
   }
 
   @Override
   public void verifyPassword(String password, String encodedPassword) {
+    log.trace("Started verify password");
+
     if (!passwordEncoder.matches(password, encodedPassword)) {
+      log.warn("Password was not verify");
       throw new PasswordsAreNotEqualException();
     }
+    log.info("Password was verified");
   }
 
-  @Transactional
   @Override
+  @Transactional
+  @CacheEvict(value = "contacts", key = "#userDetails.getUsername()")
   public void updateContactProfile(
       EditContactProfileRequestDto editContactProfileRequestDto, UserDetails userDetails) {
-    log.trace("Started updating contact profile: {}", editContactProfileRequestDto);
+    log.trace("Started updating contact profile: [{}]", editContactProfileRequestDto);
+
     String email = userDetails.getUsername();
-    Contact contact =
-        contactRepository
-            .findByEmailIgnoreCase(email)
-            .orElseThrow(
-                () -> new ContactNotFoundException(String.format("Email %s wasn't found", email)));
+
+    Contact contact = findByEmail(email);
 
     contact.setNickname(editContactProfileRequestDto.getNickname());
     contact.setAvatarId(editContactProfileRequestDto.getAvatarId());
 
-    contact = contactRepository.save(contact);
-    log.trace("Updated contact: {}", contact);
+    contactRepository.save(contact);
+
+    log.info("Updated contact by email [{}]", email);
   }
 
   @Override
   public boolean isEmailExists(String email) {
+    log.trace("Started check is email exists in repository");
     return contactRepository.existsByEmailIgnoreCase(email);
   }
 
   @Override
   public ContactProfileResponseDto getContactProfile(UserDetails userDetails) {
     String email = userDetails.getUsername();
-    Contact contact =
-        contactRepository
-            .findByEmailIgnoreCase(email)
-            .orElseThrow(
-                () -> new ContactNotFoundException(String.format("Email %s wasn't found", email)));
+    log.trace("Started get contact profile by email [{}]", email);
+
+    Contact contact = findByEmail(email);
     ContactProfileResponseDto responseDto = new ContactProfileResponseDto();
 
     responseDto.setNickname(contact.getNickname());
@@ -106,23 +127,30 @@ public class ContactServiceImpl implements ContactService {
     responseDto.setEmail(email);
     responseDto.setHasPermissionSendingPrivateMessage(contact.isPermittedSendingPrivateMessage());
 
+    log.info("Contact profile was got by email [{}]", email);
     return responseDto;
   }
 
   @Override
   @Transactional
+  @CacheEvict(value = "contacts", key = "#userDetails.getUsername()")
   public void permitSendingPrivateMessages(UserDetails userDetails) {
+    log.trace("Started permit sending private messages by email [{}]", userDetails.getUsername());
     boolean isPermittedSendingPrivateMessage = true;
 
     changePermissionSendingPrivateMessages(userDetails, isPermittedSendingPrivateMessage);
+    log.info("Permitted sending private messages by email [{}]", userDetails.getUsername());
   }
 
   @Override
   @Transactional
+  @CacheEvict(value = "contacts", key = "#userDetails.getUsername()")
   public void prohibitSendingPrivateMessages(UserDetails userDetails) {
+    log.trace("Started prohibit sending private messages by email [{}]", userDetails.getUsername());
     boolean isPermittedSendingPrivateMessage = false;
 
     changePermissionSendingPrivateMessages(userDetails, isPermittedSendingPrivateMessage);
+    log.info("Prohibited sending private messages by email [{}]", userDetails.getUsername());
   }
 
   private void changePermissionSendingPrivateMessages(
