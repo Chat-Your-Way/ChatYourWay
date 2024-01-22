97893da4a570d205986fa99093b16919f907f02a
Dmytro Trotsenko <trohae7@gmail.com>
2024-01-22 21:18:09 +0200

    fix: add password validation

--- src/main/java/com/chat/yourway/dto/request/ChangePasswordDto.java
+++ src/main/java/com/chat/yourway/dto/request/ChangePasswordDto.java
@@ -1,11 +1,15 @@
 package com.chat.yourway.dto.request;
 
+import com.chat.yourway.annotation.PasswordValidation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class ChangePasswordDto {
+    @PasswordValidation
     private String oldPassword;
+
+    @PasswordValidation
     private String newPassword;
 }
