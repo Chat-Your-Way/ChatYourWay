a74a7246f59b29c43b7703c1975ca19d569f551b
Dmytro Trotsenko <trohae7@gmail.com>
2024-02-22 19:43:53 +0200

    fix: fixed CORS and added new SMTP host

--- src/main/java/com/chat/yourway/config/websocket/WebSocketConfig.java
+++ src/main/java/com/chat/yourway/config/websocket/WebSocketConfig.java
@@ -23,6 +23,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint(properties.getEndpoint());
-    registry.addEndpoint(properties.getEndpoint()).withSockJS();
+    registry.addEndpoint(properties.getEndpoint()).setAllowedOriginPatterns("*").withSockJS();
   }
 }
