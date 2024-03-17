518fe566dbcebdcddf4e891883af5cde38840730
Dmytro Trotsenko <trohae7@gmail.com>
2024-03-17 11:52:30 +0200

    bugfix: fixed disconnect from websocket. Set time to first message.

--- src/main/java/com/chat/yourway/config/websocket/WebSocketConfig.java
+++ src/main/java/com/chat/yourway/config/websocket/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -25,4 +26,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     registry.addEndpoint(properties.getEndpoint());
     registry.addEndpoint(properties.getEndpoint()).setAllowedOriginPatterns("*").withSockJS();
   }
+
+
+  @Override
+  public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+    registry.setTimeToFirstMessage(properties.getTimeToFirstMessage());
+  }
+
 }
