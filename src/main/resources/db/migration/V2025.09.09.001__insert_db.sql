4fe3563d6ed15f65bab0a82307876e7f6c365f32
Vladik <v4054741@gmail.com>
2024-09-09 17:42:35 +0300

    new: Created UserTest for db

---
+++ src/main/resources/db/migration/V2025.09.09.001__insert_db.sql
@@ -0,0 +1,4 @@
+INSERT INTO chat.contacts(id, nickname, password, email, role, avatar_id, is_active,
+                     is_permitted_sending_private_message)
+VALUES ('550e8400-e29b-41d4-a716-446655440001', 'UserTest', 'userTestPassword!123', 'userTest@email.com', 'USER', 1,
+        true, true)
