6d91e08f4b604915b434999f7030f34c07bf35a3
Lev <lev.lvi2@gmail.com>
2024-09-12 20:47:22 +0300

    ChatController and WebSocketEventListener

---
+++ src/main/java/com/chat/yourway/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.chat.yourway.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/chat/yourway/config/websocket/WebSocketEventListener.java
@@ -0,0 +1,17 @@
+package com.chat.yourway.config.websocket;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent  event){
+
+    }
+}
