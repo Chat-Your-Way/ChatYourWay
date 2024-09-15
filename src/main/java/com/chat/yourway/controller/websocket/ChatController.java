a37bdf7f0d3ab3b2a5832bd94a6fadab328a17b0
Lev <lev.lvi2@gmail.com>
2024-09-15 12:22:34 +0300

    ChatController

--- src/main/java/com/chat/yourway/controller/websocket/ChatController.java
+++ src/main/java/com/chat/yourway/controller/websocket/ChatController.java
@@ -1,9 +1,24 @@
 package com.chat.yourway.controller.websocket;
 
 
+import com.chat.yourway.dto.response.MessageResponseDto;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/topic/public/{topicId}")
+    public void sendMessage(@DestinationVariable String topicId, MessageResponseDto message) {
+
+        messagingTemplate.convertAndSend("/topic/public/" + topicId, message);
+    }
 }
