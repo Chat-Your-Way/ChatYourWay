4e3a31e1b2e252e41b3961096230e358c58bd563
Vladik-gif <v4054741@gmail.com>
2024-10-03 17:59:52 +0300

    updated: V2025.09.09.001__insert_db.sql

--- src/main/resources/db/migration/V2025.09.09.001__insert_db.sql
+++ src/main/resources/db/migration/V2025.09.09.001__insert_db.sql
@@ -1,4 +1,5 @@
 INSERT INTO chat.contacts(id, nickname, password, email, role, avatar_id, is_active,
                      is_permitted_sending_private_message)
-VALUES ('550e8400-e29b-41d4-a716-446655440001', 'UserTest', 'Password!1', 'userTest@email.com', 'USER', 1,
-        true, true)
+VALUES ('550e8400-e29b-41d4-a716-446655440001', 'UserTest',
+        '$2y$10$oq8a2T5oMAzdWXjNnrf.u.A9pzPPZH20mm4OlCOqSaD1Z8OUBvJay',
+        'userTest@email.com', 'USER', 1, true, true)
