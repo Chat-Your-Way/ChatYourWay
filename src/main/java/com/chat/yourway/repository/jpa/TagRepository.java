30986e643be97e541960e79dcfe06d71984f5f16
Vladik-gif <v4054741@gmail.com>
2024-10-05 15:12:28 +0300

    update: Updated Integer in UUID

--- src/main/java/com/chat/yourway/repository/jpa/TagRepository.java
+++ src/main/java/com/chat/yourway/repository/jpa/TagRepository.java
@@ -2,10 +2,12 @@ package com.chat.yourway.repository.jpa;
 
 import com.chat.yourway.model.Tag;
 import java.util.Set;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TagRepository extends JpaRepository<Tag, Integer> {
+public interface TagRepository extends JpaRepository<Tag, UUID> {
   Set<Tag> findAllByNameIn(Set<String> name);
 }
