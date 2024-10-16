766a9ce4bba4bb21bce3968d365b75aa1100c504
Vladik-gif <v4054741@gmail.com>
2024-10-16 15:35:13 +0300

    update: ContactRepository

--- src/main/java/com/chat/yourway/repository/jpa/ContactRepository.java
+++ src/main/java/com/chat/yourway/repository/jpa/ContactRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContactRepository extends JpaRepository<Contact, Integer> {
+public interface ContactRepository extends JpaRepository<Contact, UUID> {
 
   Optional<Contact> findByEmailIgnoreCase(String email);
 
