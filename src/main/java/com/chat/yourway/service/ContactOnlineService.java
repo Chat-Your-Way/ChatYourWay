30c417483f17b7126cd2c666870e4bde92c07406
Vladik-gif <v4054741@gmail.com>
2024-10-17 09:56:11 +0300

    update: ContactOnlineService.setUserOnline for .typingStatus(false)

--- src/main/java/com/chat/yourway/service/ContactOnlineService.java
+++ src/main/java/com/chat/yourway/service/ContactOnlineService.java
@@ -33,7 +33,7 @@ public class ContactOnlineService {
                 .id(contactEmail)
                 .timestamp(LocalDateTime.now())
                 .topicId(topicId)
-                .typingStatus(true)
+                .typingStatus(false)
                 .build();
         contactOnlineRedisRepository.save(contactOnline);
         notificationService.contactChangeStatus(getOnlineContacts(), contactService.findByEmail(contactEmail), contactOnline);
