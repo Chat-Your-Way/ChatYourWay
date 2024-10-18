bede56149ea5d9a4301b3b2434ea6d00176a8e47
Vladik-gif <v4054741@gmail.com>
2024-10-18 11:13:49 +0300

    Added test user

--- src/main/resources/db/migration/V2025.09.09.001__insert_db.sql
+++ src/main/resources/db/migration/V2025.09.09.001__insert_db.sql
@@ -1,5 +1,9 @@
 INSERT INTO chat.contacts(id, nickname, password, email, role, avatar_id, is_active,
                      is_permitted_sending_private_message)
-VALUES ('550e8400-e29b-41d4-a716-446655440001', 'UserTest',
+VALUES ('550e8400-e29b-41d4-a716-446655440001', 'KostiantynTest',
         '$2y$10$oq8a2T5oMAzdWXjNnrf.u.A9pzPPZH20mm4OlCOqSaD1Z8OUBvJay',
-        'userTest@email.com', 'USER', 1, true, true)
+        'kostiantynTest@email.com', 'USER', 1, true, true),
+
+    ('550e8400-e29b-41d4-a716-446655440002', 'VladikTest',
+        '$2y$10$oq8a2T5oMAzdWXjNnrf.u.A9pzPPZH20mm4OlCOqSaD1Z8OUBvJay',
+        'vladikTest@email.com', 'USER', 2, true, true)
